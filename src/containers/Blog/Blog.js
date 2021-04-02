9fc61ca7f9efacbc306e031574680f30ec64998d
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-04-02 18:10:55 +0100

    feat: making a post selectable

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
         state = {
-            posts: []
+            posts: [],
+            selectedPostId: null
         }
 
 
@@ -16,13 +17,28 @@ class Blog extends Component {
         componentDidMount () {
             axios.get('https://jsonplaceholder.typicode.com/posts')
                 .then(response => {
-                    this.setState({posts: response.data});
-                 //   console.log(response);
-                });
-        }
-    render () {
+                    const posts = response.data.slice(0, 4);
+                    const updatedPosts = posts.map(post => {
+                        return {
+                            ...post,
+                            author: 'Patryk'
+                        }
+                    });
+                    this.setState({posts: updatedPosts});
+        });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
+        render () {
         const posts = this.state.posts.map(post => {
-            return <Post title={post.title} />
+            return <Post 
+                key={post.id}  
+                title={post.title} 
+                author={post.author} 
+                clicked={() => this.postSelectedHandler(post.id)}/>;
         });
 
         return (
@@ -31,7 +47,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
