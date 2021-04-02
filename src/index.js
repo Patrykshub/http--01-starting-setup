af4af15d7109ea42db64103e04b218fdf8fc4c5f
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-04-02 20:14:08 +0100

    feat: default global configs for axios

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN'
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 axios.interceptors.request.use(request => {
     console.log(request);
     //we have to return it otherwise its blocked. yyou can edit request config, before we return it. for example: headers
